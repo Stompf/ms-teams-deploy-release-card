698bc345c095941a64209322ffefea0097e1bc24
Anton Lundmark <anton.lundmark@axis.com>
2021-10-15 16:07:59 +0200

    Removed github-owner input

--- src/options.ts
+++ src/options.ts
@@ -4,13 +4,16 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export function getOptions() {
+  const repoFullName = core.getInput('github-repo').split('/');
+
   return {
     githubToken: core.getInput('github-token'),
-    githubOwner: core.getInput('github-owner'),
-    githubRepo: core.getInput('github-repo'),
+    githubOwner: repoFullName[0],
+    githubRepo: repoFullName[1],
     githubTag: core.getInput('github-tag'),
     msTeamsWebHookUrl: core.getInput('ms-teams-webhook-url'),
-    msTeamsCardTitle: core.getInput('ms-teams-card-title') || core.getInput('github-tag'),
+    msTeamsCardTitle:
+      core.getInput('ms-teams-card-title') || `${core.getInput('github-repo')} - ${core.getInput('github-tag')}`,
     msTeamsCardThemeColor: core.getInput('ms-teams-card-theme-color'),
     anonymize: core.getInput('anonymize').toLowerCase().trim() === 'true',
   };
